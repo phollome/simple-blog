c0ea424050783c8e79e2c3bb9dbdfee7e2364b77
Peter Holló <hello@songsforthe.dev>
2023-06-16 02:30:41 +0200

    subset of file info (no stats necessary)

--- src/utils/fs.spec.ts
+++ src/utils/fs.spec.ts
@@ -1,9 +1,19 @@
 import { test, expect } from "vitest";
-import { getFilePaths, getFileInfo } from "./fs";
+import { getFilePaths, getFileInfo, getFilenameInfo } from "./fs";
 import fs from "fs-extra";
 
-test("getListOfFilePaths()", async () => {
+test("getFilePaths()", async () => {
   const files = await getFilePaths("./tmp");
+
+  expect(files).toEqual([
+    "./tmp/files/more-files/another-markdown-file.md",
+    "./tmp/files/some-markdown-file.md",
+    "./tmp/no-mds/some-text-file.txt",
+  ]);
+});
+
+test("getFilePaths() + ignore extensions", async () => {
+  const files = await getFilePaths("./tmp", { ignoreExtensions: [".txt"] });
 
   expect(files).toEqual([
     "./tmp/files/more-files/another-markdown-file.md",
@@ -11,7 +21,17 @@ test("getListOfFilePaths()", async () => {
   ]);
 });
 
-test("getStatsOfFile()", async () => {
+test("getFilenameInfo()", () => {
+  const testFilePath = "./tmp/get-stats-of-file.txt";
+
+  const info = getFilenameInfo(testFilePath);
+
+  expect(info.filename).toBe("get-stats-of-file.txt");
+  expect(info.filenameWithoutExtension).toBe("get-stats-of-file");
+  expect(info.extension).toBe(".txt");
+});
+
+test("getFileInfo()", async () => {
   const testFilePath = "./tmp/get-stats-of-file.txt";
 
   await fs.ensureFile(testFilePath);
@@ -21,6 +41,7 @@ test("getStatsOfFile()", async () => {
   expect(info.createdAt).toBeInstanceOf(Date);
   expect(info.updatedAt).toBeInstanceOf(Date);
   expect(info.filename).toBe("get-stats-of-file.txt");
+  expect(info.filenameWithoutExtension).toBe("get-stats-of-file");
   expect(info.extension).toBe(".txt");
 
   await fs.remove(testFilePath);
--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,15 +1,29 @@
 import fs from "fs-extra";
 import path from "path";
 
-export async function getFilePaths(root: string) {
+export async function getFilePaths(
+  root: string,
+  options?: { ignoreExtensions?: string[] }
+) {
   const entries = await fs.readdir(root, { withFileTypes: true });
 
   const files: string[] = [];
   for (const entry of entries) {
+    const filePath = `${root}/${entry.name}`;
+
     if (entry.isDirectory()) {
-      files.push(...(await getFilePaths(`${root}/${entry.name}`)));
-    } else if (entry.name.endsWith(".md")) {
-      files.push(`${root}/${entry.name}`);
+      files.push(...(await getFilePaths(filePath, options)));
+    } else {
+      if (options !== undefined && options.ignoreExtensions !== undefined) {
+        const ignore = options.ignoreExtensions.some((ext) => {
+          return entry.name.endsWith(ext);
+        });
+        if (!ignore) {
+          files.push(filePath);
+        }
+      } else {
+        files.push(filePath);
+      }
     }
   }
 
@@ -18,12 +32,20 @@ export async function getFilePaths(root: string) {
 
 export async function getFileInfo(filePath: string) {
   const stats = await fs.stat(filePath);
-  const filename = path.basename(filePath);
-  const extension = path.extname(filePath);
   return {
     createdAt: stats.birthtime,
     updatedAt: stats.mtime,
+    ...getFilenameInfo(filePath),
+  };
+}
+
+export function getFilenameInfo(filePath: string) {
+  const filename = path.basename(filePath);
+  const extension = path.extname(filePath);
+  const filenameWithoutExtension = path.basename(filePath, extension);
+  return {
     filename,
     extension,
+    filenameWithoutExtension,
   };
 }
